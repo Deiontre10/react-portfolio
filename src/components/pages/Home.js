28e63096ca04639abe85b5abb7f8366bcaa3d437
Deiontre10 <travontazlowry@gmail.com>
2023-02-17 11:56:00 -0500

    updated Home.js

---
+++ src/components/pages/Home.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "/.footer";
+import pageComponents from "./pages";
+
+const pages = Object.keys(pageComponents);
+
+export default function Homepage() {
+    const [currentPage, setCurrentPage] = useState("About");
+    const Page = () => pageComponents[currentPage]();
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <>
+            <Header 
+                handlePageChange={handlePageChange}
+                pages={pages}
+                currentPage={currentPage}
+            />
+            <Page className="m-3 p-2 d-flex container min-vh-100"/>
+            <Footer />
+        </>
+    )
+}
