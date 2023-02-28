7b2a57a16f8c8aa84d5b923ecefc0d198b4b4fb1
Deiontre10 <travontazlowry@gmail.com>
2023-02-28 17:01:06 -0500

    updated About.jsx

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
         <div className="pages">
             <h2>About Me</h2>
             <div className="container">
-                <p>Hello</p>
+                <p>Hello, I am Travontaz/Taz Lowry. I am a Full-Stack Web Developer looking forward to facing challenges. I hope to grow in this new role and have fun doing something that I love. Please check out the projects that I have highlighted here in my portfolio. Do not hesitate to reach out if you would like to connect.</p>
             </div>
         </div>
     )
