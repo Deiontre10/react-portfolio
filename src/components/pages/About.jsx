9abc4c92cd14c4fdece6695d767011f35d03a7ee
Deiontre10 <travontazlowry@gmail.com>
2023-03-01 09:39:53 -0500

    updated About.jsx

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
             <h2>About Me</h2>
             <div className="row">
                 <div className="col-6">
-                    <p>Hello, I am Travontaz/Taz Lowry. I am a Full-Stack Web Developer looking forward to facing challenges. I hope to grow in this new role and have fun doing something that I love. Please check out the projects that I have highlighted here in my portfolio. Do not hesitate to reach out if you would like to connect.</p>
+                    <p className="fs-4 text-white">Hello, I am Travontaz/Taz Lowry. I am a Full-Stack Web Developer looking forward to facing challenges. I hope to grow in this new role and have fun doing something that I love. Please check out the projects that I have highlighted here in my portfolio. Do not hesitate to reach out if you would like to connect.</p>
                 </div>
                 <div className="col-4">
                     <img src={profile} alt="Taz Lowry" className="profile-picture" />
